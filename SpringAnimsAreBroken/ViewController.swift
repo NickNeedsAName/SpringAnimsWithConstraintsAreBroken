80cc85c9b68f9578c39005cac579607d5edfdb73
NickNeedsAName <ng1213@gmail.com>
2024-06-25 13:50:59 -0700

    Initial Commit

---
+++ SpringAnimsAreBroken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAnimsAreBroken
+//
+//  Created by Nick Grabenstein on 6/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
